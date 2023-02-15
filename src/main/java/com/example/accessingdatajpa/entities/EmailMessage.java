2efc0626b58e72a31a4b831974e9190fe9fa79a1
vuyo <joymama078@gmail.com>
2023-02-15 10:09:16 +0200

    Create EmailMessage entity

--- src/main/java/com/example/accessingdatajpa/entities/EmailMessage.java
+++ src/main/java/com/example/accessingdatajpa/entities/EmailMessage.java
@@ -1,88 +1,97 @@
 package com.example.accessingdatajpa.entities;
 
 
+import jakarta.persistence.*;
 
+@Entity
 public class EmailMessage {
 
-  private long id;
-  private String body;
-  private String emailSubject;
-  private java.sql.Timestamp sentDate;
-  private long senderId;
-  private long recipientId;
-  private String hasAttachments;
-  private String isDraft;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String body;
+    private String emailSubject;
+    private java.sql.Timestamp sentDate;
+
+    @OneToOne
+    @JoinColumn(name = "senderId")
+    private User sender;
+
+    @OneToOne
+    @JoinColumn(name = "recipientId")
+    private User recipient;
+    private String hasAttachments;
+    private String isDraft;
 
 
-  public long getId() {
-    return id;
-  }
+    public long getId() {
+        return id;
+    }
 
-  public void setId(long id) {
-    this.id = id;
-  }
-
-
-  public String getBody() {
-    return body;
-  }
-
-  public void setBody(String body) {
-    this.body = body;
-  }
+    public void setId(long id) {
+        this.id = id;
+    }
 
 
-  public String getEmailSubject() {
-    return emailSubject;
-  }
+    public String getBody() {
+        return body;
+    }
 
-  public void setEmailSubject(String emailSubject) {
-    this.emailSubject = emailSubject;
-  }
-
-
-  public java.sql.Timestamp getSentDate() {
-    return sentDate;
-  }
-
-  public void setSentDate(java.sql.Timestamp sentDate) {
-    this.sentDate = sentDate;
-  }
+    public void setBody(String body) {
+        this.body = body;
+    }
 
 
-  public long getSenderId() {
-    return senderId;
-  }
+    public String getEmailSubject() {
+        return emailSubject;
+    }
 
-  public void setSenderId(long senderId) {
-    this.senderId = senderId;
-  }
-
-
-  public long getRecipientId() {
-    return recipientId;
-  }
-
-  public void setRecipientId(long recipientId) {
-    this.recipientId = recipientId;
-  }
+    public void setEmailSubject(String emailSubject) {
+        this.emailSubject = emailSubject;
+    }
 
 
-  public String getHasAttachments() {
-    return hasAttachments;
-  }
+    public java.sql.Timestamp getSentDate() {
+        return sentDate;
+    }
 
-  public void setHasAttachments(String hasAttachments) {
-    this.hasAttachments = hasAttachments;
-  }
+    public void setSentDate(java.sql.Timestamp sentDate) {
+        this.sentDate = sentDate;
+    }
 
 
-  public String getIsDraft() {
-    return isDraft;
-  }
+    public User getSender() {
+        return sender;
+    }
 
-  public void setIsDraft(String isDraft) {
-    this.isDraft = isDraft;
-  }
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+
+    public String getHasAttachments() {
+        return hasAttachments;
+    }
+
+    public void setHasAttachments(String hasAttachments) {
+        this.hasAttachments = hasAttachments;
+    }
+
+
+    public String getIsDraft() {
+        return isDraft;
+    }
+
+    public void setIsDraft(String isDraft) {
+        this.isDraft = isDraft;
+    }
 
 }
