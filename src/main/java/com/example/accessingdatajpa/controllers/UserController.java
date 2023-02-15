28a29f3bb6bf32e02df4e7014925f7c54001508d
vuyo <joymama078@gmail.com>
2023-02-15 15:37:51 +0200

    Create PropertiesController

--- src/main/java/com/example/accessingdatajpa/controllers/UserController.java
+++ src/main/java/com/example/accessingdatajpa/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.example.accessingdatajpa.controllers;
 
+import com.example.accessingdatajpa.dto.ResponseUser;
 import com.example.accessingdatajpa.entities.Property;
 import com.example.accessingdatajpa.entities.User;
 import com.example.accessingdatajpa.entities.UserEmail;
+import com.example.accessingdatajpa.entities.UserType;
 import com.example.accessingdatajpa.repositories.PropertyRepository;
 import com.example.accessingdatajpa.repositories.UserEmailRepository;
 import com.example.accessingdatajpa.repositories.UserRepository;
+import com.example.accessingdatajpa.repositories.UserTypeRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,17 +20,21 @@ import java.util.Optional;
 
 import static com.example.accessingdatajpa.Utils.parseId;
 
-
 @RestController
 public class UserController {
     private final UserRepository userRepository;
     private final UserEmailRepository userEmailRepository;
     private final PropertyRepository propertyRepository;
+    private final UserTypeRepository userTypeRepository;
 
-    public UserController(UserRepository userRepository, UserEmailRepository userEmailRepository, PropertyRepository propertyRepository) {
+    public UserController(UserRepository userRepository,
+                          UserEmailRepository userEmailRepository,
+                          PropertyRepository propertyRepository,
+                          UserTypeRepository userTypeRepository) {
         this.userRepository = userRepository;
         this.userEmailRepository = userEmailRepository;
         this.propertyRepository = propertyRepository;
+        this.userTypeRepository = userTypeRepository;
     }
 
     private Optional<User> getUser(long userId) {
@@ -38,36 +45,79 @@ public class UserController {
         return Optional.of(user);
     }
 
-    @GetMapping(value = "/users/", produces = MediaType.APPLICATION_JSON_VALUE)
+    private Optional<Property> getProperty(long propertyId) {
+        Property property = propertyRepository.getPropertiesById(propertyId);
+
+        if (property == null) return Optional.empty();
+
+        return Optional.of(property);
+    }
+
+    private void setUpUser(User user) {
+        long propertyId = user.getProperty().getId();
+        Optional<Property> property = getProperty(propertyId);
+
+        if (property.isEmpty()) throw new RuntimeException(String.format("No such Property with id: %d", propertyId));
+
+        user.setProperty(property.get());
+
+        LocalDateTime now = LocalDateTime.now();
+        user.setDateJoined(Timestamp.valueOf(now));
+
+        userRepository.save(user);
+    }
+
+    @GetMapping(value = "/tenants/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Iterable<User>> getAllUsers() {
         Iterable<User> users = userRepository.findAll();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/users/{userId}/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<User> getUserById(@PathVariable("userId") String pathVariable) {
+    @GetMapping(value = "/tenants/{tenantId}/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseUser> getUserById(@PathVariable("tenantId") String pathVariable) {
         Optional<Long> userId = parseId(pathVariable);
 
         if (userId.isEmpty()) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
         Optional<User> user = getUser(userId.get());
-        return user.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        if (user.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        UserType userType = userTypeRepository.getUserTypeByUserId(user.get().getId());
+        ResponseUser responseUser = new ResponseUser(user.get(), userType);
+
+        return new ResponseEntity<>(responseUser, HttpStatus.OK);
     }
 
-    @PostMapping(value = "/users/register/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<User> createNewUser(@RequestBody User user) {
-        Property property = propertyRepository.getPropertiesById(user.getProperty().getId());
-        LocalDateTime now = LocalDateTime.now();
-        user.setDateJoined(Timestamp.valueOf(now));
+    @PostMapping(value = "/tenants/register/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> registerTenant(@RequestBody User user) {
+        try {
+            setUpUser(user);
+        } catch (RuntimeException runtimeException) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
-        user.setProperty(property);
-        userRepository.save(user);
+        UserType userType = new UserType(user, "tenant");
+        userTypeRepository.save(userType);
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @GetMapping(value = "/users/{userId}/emails/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Iterable<UserEmail>> getUserEmails(@PathVariable("userId") String pathVariable) {
+    @PostMapping(value = "/admin/register/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> registerAdminUser(@RequestBody User user) {
+        try {
+            setUpUser(user);
+        } catch (RuntimeException runtimeException) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        UserType userType = new UserType(user, "admin");
+        userTypeRepository.save(userType);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/tenants/{tenantId}/emails/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Iterable<UserEmail>> getTenantEmails(@PathVariable("tenantId") String pathVariable) {
         Optional<Long> userId = parseId(pathVariable);
 
         if (userId.isEmpty()) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
